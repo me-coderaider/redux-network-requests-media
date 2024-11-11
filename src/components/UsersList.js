b1446effbeed45dd01a95b2ad0467c3b5f2d3d51
me-coderaider <kashyappankajnsp@gmail.com>
2024-11-11 13:46:28 +0530

    Using OPTION 1 for 'Fine Grained Loading State' in UsersList component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,30 +1,60 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
 
 function UsersList() {
+    const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+    const [loadingUsersError, setLoadingUsersError] = useState(null);
+    const [isCreatingUser, setIsCreatingUser] = useState(false);
+    const [creatingUserError, setIsCreatingUserError] = useState(null);
+
     const dispatch = useDispatch();
 
-    const { isLoading, data, error } = useSelector((state) => {
+    // const { isLoading, data, error } = useSelector((state) => {
+    const { data } = useSelector((state) => {
         return state.users; // {data:[], isLoading:false, error:null}
     });
 
     useEffect(() => {
-        dispatch(fetchUsers());
+        setIsLoadingUsers(true);
+        dispatch(fetchUsers())
+            .unwrap()
+            .then(() => {
+                // setIsLoadingUsers(false);
+                // console.log("Success");
+            })
+            .catch((err) => {
+                setLoadingUsersError(err);
+                // setIsLoadingUsers(false);
+                // console.log("Fail");
+            })
+            .finally(() => {
+                setIsLoadingUsers(false);
+            });
     }, [dispatch]);
 
     const handleAddUser = () => {
-        dispatch(addUser());
+        setIsCreatingUser(true);
+        dispatch(addUser())
+            .unwrap()
+            .catch((err) => {
+                setIsCreatingUser(err);
+            })
+            .finally(() => {
+                setIsCreatingUser(false);
+            });
     };
 
-    if (isLoading) {
+    // if (isLoading) {
+    if (isLoadingUsers) {
         // return <div>Loading...</div>;
         return <Skeleton times={6} className="h-10 w-full" />;
     }
 
-    if (error) {
+    // if (error) {
+    if (loadingUsersError) {
         return <div>Error fetching data...</div>;
     }
 
@@ -42,7 +72,12 @@ function UsersList() {
         <div>
             <div className="flex flex-row justify-between m-3">
                 <h1 className="m-2 text-xl">Users</h1>
-                <Button onClick={handleAddUser}>+ Add User</Button>
+                {isCreatingUser ? (
+                    "Creating User..."
+                ) : (
+                    <Button onClick={handleAddUser}>+ Add User</Button>
+                )}
+                {creatingUserError && "Error Creating user..."}
             </div>
             {renderedUsers}
         </div>
