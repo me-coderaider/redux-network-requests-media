d3593d0a03c291bfccd2f12bf3d3e59e88959adc
me-coderaider <kashyappankajnsp@gmail.com>
2024-11-12 11:37:30 +0530

    working on 'AlbumList' and generating new Album using 'Mutations' from 'albumsApi'

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,41 @@
-import { useFetchAlbumsQuery } from "../store";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
 
 function AlbumList({ user }) {
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
+    const [addAlbum, results] = useAddAlbumMutation();
 
-    console.log(data, error, isLoading);
-    return <div>Albums for {user.name}</div>;
+    const handleAddAlbum = () => {
+        addAlbum(user);
+    };
+
+    let content;
+    if (isLoading) {
+        content = <Skeleton times={3} />;
+    } else if (error) {
+        content = <div>Error loading albums.</div>;
+    } else {
+        content = data.map((album) => {
+            const header = <div>{album.title}</div>;
+            return (
+                <ExpandablePanel key={album.id} header={header}>
+                    List of photos in the album
+                </ExpandablePanel>
+            );
+        });
+    }
+
+    return (
+        <div>
+            <div>
+                Albums for {user.name}
+                <Button onClick={handleAddAlbum}>+ Add Album</Button>
+            </div>
+            <div>{content}</div>
+        </div>
+    );
 }
 
 export default AlbumList;
